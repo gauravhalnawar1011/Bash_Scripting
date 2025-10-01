f18f79ed3e9ecbe19aabf8a5013f213b6b70b228
gauravhalnawar1011 <gauravhalnawar1011@example.com>
2025-10-01 18:10:22 +0530

    the array and string operations

--- array_in_bash/array.sh
+++ array_in_bash/array.sh
@@ -1,6 +1,29 @@
 #/bin/bash
 
+
 array=(1 "hello world" 39.09 "hey rokke" "hey dev")
 
+students=("rahul" "vishal" "shravn")
+roll_no=(12 34 56)
 
-echo "${array[@]}"
+echo "the stduents name is the: ${students[@]}"
+
+echo "the roll number of the studnets are : ${roll_no[@]}"
+
+echo "the index 0 has the value: ${students[0]}"
+echo "the index 1 has the value: ${students[1]}"
+echo "the index 2 has the value: ${students[2]}"
+echo "the index 3 has the value: ${students[3]}"
+echo "the index 0 has the value: ${students[*]:1:3}"
+
+
+roll_no+=(67 89 09)
+
+echo "update new roll_no list : ${roll_no[@]}"
+
+# echo "${array[@]}"
+# to check the array length 
+
+echo  "the the lngth of an roll_no is : ${#roll_no[*]}"
+
+echo  "the the lngth of an students  is : ${#students[*]}"
--- array_in_bash/key-value-array.sh
+++ array_in_bash/key-value-array.sh
@@ -5,6 +5,14 @@ declare -A myArray
 
 myArray=([name]=Gaurav [height]=6ft [weight]=85kg [favrote]=banana [food]=aloo)
 
+
+declare -A students
+students=([name]=Rahul [roll_no]=34 [university]=SPPU)
+
+echo "the my studnets array conatins an name is : ${students[name]}"
+echo "the my studnets array conatins an roll no  is : ${students[roll_no]}"
+echo "the my studnets array conatins an university is : ${students[university]}"
+
 echo "${myArray[*]}"
 
 echo "${myArray[name]}"
